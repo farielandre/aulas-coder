464dff79619de7003621dd764c9ff77d2ffaf63a
Fariel Minozzo <farielandre@gmail.com>
2023-12-18 21:39:18 -0300

    Aula logica de programacao

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// alert("Hello world");
+// console.log("Hello World");
+// console.log(prompt("Qual seu nome:"));
+
+/*
+const myConst = 50;
+let soma;
+soma = myConst + 20;
+
+console.log(soma);
+*/
+
+/*
+const name = "Fariel"
+const numero1 = 2;
+const numero2 = 26;
+const sum = numero1 + numero2;
+const multplication = numero1 * numero2;
+
+console.log(prompt("Qual seu nome:"));
+*/
+
+
+// Atribuindo valores às variáveis
+
+/* franquia x - soma de horas totais de duas semanas trabalhadas na empresa.
+e também multiplicação de horas totais (soma) pelo valor por hora do freelancer.
+
+function somaTrabalho (horasTrabalhadasSemana1, horasTrabalhadasSemana2) {
+    return horasTrabalhadasSemana1 + horasTrabalhadasSemana2;
+}
+
+function multiplicacao (somaTrabalho) {
+    return somaTrabalho * 80;
+}
+
+const nomeTrabalho = "Franquia X"
+let horasTrabalhadasSemana1 = 30;
+let horasTrabalhadasSemana2 = 40;
+let somaDaFuncao = somaTrabalho(horasTrabalhadasSemana1, horasTrabalhadasSemana2)
+console.log(multiplicacao(somaDaFuncao))
+
+*/
+
+function multiplica(num1, num2) {
+    let result = num1 * num2;
+    console.log(result);
+}
+
+multiplica(4, 7);
+multiplica(20, 20);
+multiplica(0.5, 3);
